fa2289840af786a4826437df2e8ed9e5d0fd4d67
duruchima <duruchima1@gmail.com>
2020-09-04 14:35:18 -0400

    added CRUD actions to Order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,44 @@
 class OrdersController < ApplicationController
+    before_action :find_order,only: [:show, :edit, :update]
+	
+    def index
+        @orders=Order.all
+        render json:@orders
+    end
+
+    def show
+        render json:@order
+    end
+    
+
+
+    def create
+        @order= Order.new(order_params)
+        @order.save
+
+        render json:@order
+    end
+    
+    def update
+        @order.update(params.require(:order).permit!)
+        if @order.valid?
+            render json:@order
+        else
+            flash[:errors]=@order.erors.full_messages
+        end
+    end
+
+    def delete 
+        Order.find(params[:id]).delete
+    end
+
+    private
+
+    def order_params
+        params.require(:order).permit(:user_id, :bakery_id, :price, :dough)
+    end
+
+    def find_order
+        @order = Order.find(params[:id])
+    end
 end
