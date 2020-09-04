14d5d3262da9f1f7f4455450792270ec49e89a83
duruchima <duruchima1@gmail.com>
2020-09-04 14:22:07 -0400

    added functions to address controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,44 @@
 class AddressesController < ApplicationController
+    before_action :find_address,only: [:show, :edit, :update]
+	
+    def index
+        @addresses=Address.all
+        render json:@addresses
+    end
+
+    def show
+        render json:@address
+    end
+    
+
+
+    def create
+        @address= Address.new(address_params)
+        @address.save
+
+        render json:@address
+    end
+    
+    def update
+        @address.update(params.require(:address).permit!)
+        if @address.valid?
+            render json:@address
+        else
+            flash[:errors]=@address.erors.full_messages
+        end
+    end
+
+    def delete 
+        Address.find(params[:id]).delete
+    end
+
+    private
+
+    def address_params
+        params.require(:address).permit(:user_id, :street, :city, :state, :zip_code)
+    end
+
+    def find_address
+        @address = Address.find(params[:id])
+    end
 end
