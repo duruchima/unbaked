23fe360f5f4513f114d6873a6f13e6b3df512ba5
JohnsonNYC <jkow95@gmail.com>
2020-09-28 16:04:06 -0400

    added more seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
+Bakery.destroy_all
 Order.destroy_all
 
 User.create(first_name: "Duncan", last_name: "Uruchima", username: "duruchima", email: "duncan@email.com", age:27, zip_code: 11111)
 User.create(first_name: "Johnson", last_name: "Kow", username: "JohnsonNYC", email: "johnballs@email.com", age:30, zip_code: 22222)
+
+
+Bakery.create(name:"Chocnyc", address:"4996 Broadway, New York, NY 10034")
+Bakery.create(name:"Orwashers Bakery", address:"308 E 78th St, New York, NY 10075")
+Bakery.create(name:"Levain Bakery", address:"167 W 74th St, New York, NY 10023")
+Bakery.create(name:"Two Little Red Hens", address:"1652 2nd Ave, New York, NY 10028")
+
+Order.create(user_id:User.all.sample.id, bakery_id:Bakery.all.sample.id, price:'9.00', dough:)
 
 
 
